66c2bb2d1bddacf1e6384ca6761cd33a74f447c2
Irene Vu <duyenvu298@gmail.com>
2018-10-17 08:33:09 -0700

    Add files via upload

---
+++ typewrite.js
@@ -0,0 +1,11 @@
+
+var typed = new Typed('#typed', {
+  stringsElement: '#typed-strings',
+  typeSpeed: 75,
+  backSpeed: 100,
+  loop: true,
+  loopCount: Infinity,
+  showCursor: true,
+  cursorChar: '|'
+ 	});
+
